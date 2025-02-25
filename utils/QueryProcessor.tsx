a95f9bd549bb96aafc9a5a7a193ac9eb1a26c97b
Ryan Chernoff <rchernof@andrew.cmu.edu>
2025-02-25 09:47:26 -0500

    feat: Changed name to match my name

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -8,7 +8,7 @@ export default function QueryProcessor(query: string): string {
   }
 
   if (query.toLowerCase().includes("name")) {
-    return "Rohan";
+    return "Ryan";
   }
 
   if (query.toLowerCase().includes("andrew id")){
